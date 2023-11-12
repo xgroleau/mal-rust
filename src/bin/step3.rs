cf28e5d6917fab626f7a3935d6b80b600fb4a8ca
Xavier Groleau <xavgroleau@gmail.com>
2023-11-12 17:28:01 -0500

    wip: step 3 ast env

--- src/bin/step3.rs
+++ src/bin/step3.rs
@@ -1,12 +1,62 @@
-extern crate rustyline;
-
+use std::collections::HashMap;
 use std::rc::Rc;
 
-use mal_rust::env::{env_set, Env};
+extern crate rustyline;
+
+use anyhow::{anyhow, Context};
+use mal_rust::env::{env_get, env_set, Env};
+use mal_rust::printer::pr_str;
+use mal_rust::reader;
 use mal_rust::types::MalValue;
-use mal_rust::{base_fn, printer, reader};
+use mal_rust::{base_fn, Result};
 use rustyline::error::ReadlineError;
-use rustyline::{DefaultEditor, Result};
+use rustyline::DefaultEditor;
+
+fn eval_ast(env: &mut Rc<Env>, ast: &MalValue) -> Result<MalValue> {
+    match ast {
+        MalValue::Sym(s) => env_get(env, s).context(anyhow!("Symbol: '{}' not found", s)),
+        MalValue::List(list) => {
+            let val = list.iter().map(|v| eval(env, v)).collect::<Result<_>>()?;
+            Ok(MalValue::List(Rc::new(val)))
+        }
+        MalValue::Vec(vec) => {
+            let val = vec.iter().map(|v| eval(env, v)).collect::<Result<_>>()?;
+            Ok(MalValue::Vec(Rc::new(val)))
+        }
+        MalValue::Map(map) => {
+            let val = map
+                .iter()
+                .map(|(k, v)| {
+                    let val = eval(env, v)?;
+                    Ok((k.clone(), val))
+                })
+                .collect::<Result<HashMap<String, MalValue>>>()?;
+            Ok(MalValue::Map(Rc::new(val)))
+        }
+        v => Ok(v.clone()),
+    }
+}
+
+fn eval(env: &mut Rc<Env>, ast: &MalValue) -> Result<MalValue> {
+    match ast {
+        MalValue::List(l) => {
+            if l.is_empty() {
+                Ok(ast.clone())
+            } else {
+                let new_ast = eval_ast(env, ast)?;
+                match new_ast {
+                    MalValue::List(list) => {
+                        let f = list[0].clone();
+                        let args = &list[1..];
+                        f.apply(Rc::new(args.to_vec()))
+                    }
+                    _ => Err(anyhow!("Didn't receive list after evaluating list")),
+                }
+            }
+        }
+        v => eval_ast(env, v),
+    }
+}
 
 fn main() -> Result<()> {
     let mut rl = DefaultEditor::new()?;
@@ -14,11 +64,12 @@ fn main() -> Result<()> {
         eprintln!("No previous history.");
     }
 
-    let env = Rc::new(Env::new());
+    let mut env = Rc::new(Env::new());
     env_set(&env, "+".to_string(), MalValue::Function(base_fn::add));
-    env_set(&env, "-".to_string(), MalValue::Function(base_fn::sub));
+    env_set(&env, "+".to_string(), MalValue::Function(base_fn::add));
     env_set(&env, "*".to_string(), MalValue::Function(base_fn::mult));
     env_set(&env, "/".to_string(), MalValue::Function(base_fn::div));
+
     loop {
         let readline = rl.readline("mal-rs> ");
         match readline {
@@ -26,10 +77,13 @@ fn main() -> Result<()> {
                 rl.add_history_entry(&line)?;
                 rl.save_history(".mal-history")?;
                 if line.len() > 0 {
-                    match reader::read_str(&line) {
-                        Ok(mv) => {
-                            println!("{}", printer::pr_str(&mv))
-                        }
+                    let res = match reader::read_str(&line) {
+                        Ok(ast) => eval(&mut env, &ast),
+                        Err(e) => Err(e),
+                    };
+
+                    match res {
+                        Ok(v) => println!("{}", pr_str(&v)),
                         Err(e) => println!("Error: {}", e),
                     }
                 }
@@ -38,7 +92,6 @@ fn main() -> Result<()> {
             Err(ReadlineError::Eof) => break,
             Err(err) => {
                 println!("Error: {:?}", err);
-                break;
             }
         }
     }
