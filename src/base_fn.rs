7819754878e0e0d5da3594749c7a3a6d38f75391
xgroleau🐢 <xavgroleau@gmail.com>
2023-11-27 18:11:57 -0500

    wip: base function for step4

--- src/base_fn.rs
+++ src/base_fn.rs
@@ -1,5 +1,8 @@
+use std::rc::Rc;
+
 use crate::{printer::pr_str, types::MalValue, Result};
 use anyhow::anyhow;
+use itertools::Itertools;
 
 pub fn add(args: &Vec<MalValue>) -> Result<MalValue> {
     match &args[..] {
@@ -43,40 +46,34 @@ pub fn div(args: &Vec<MalValue>) -> Result<MalValue> {
 }
 
 pub fn prn(args: &Vec<MalValue>) -> Result<MalValue> {
-    match &args[..] {
-        [MalValue::Number(l), MalValue::Number(r)] => Ok(MalValue::Number(l / r)),
-        a => Err(anyhow!(
-            "Cannot div args: {:?}",
-            a.iter().map(|v| pr_str(v)).collect::<Vec<_>>()
-        )),
-    }
+    let str: String = args.iter().map(|v| pr_str(v)).join(" ");
+    println!("{}", str);
+    Ok(MalValue::Nil)
 }
 
 pub fn list(args: &Vec<MalValue>) -> Result<MalValue> {
-    match &args[..] {
-        [MalValue::Number(l), MalValue::Number(r)] => Ok(MalValue::Number(l / r)),
-        a => Err(anyhow!(
-            "Cannot div args: {:?}",
-            a.iter().map(|v| pr_str(v)).collect::<Vec<_>>()
-        )),
-    }
+    Ok(MalValue::List(Rc::new(args.clone())))
 }
 
 pub fn is_list(args: &Vec<MalValue>) -> Result<MalValue> {
     match &args[..] {
-        [MalValue::Number(l), MalValue::Number(r)] => Ok(MalValue::Number(l / r)),
-        a => Err(anyhow!(
-            "Cannot div args: {:?}",
-            a.iter().map(|v| pr_str(v)).collect::<Vec<_>>()
+        [MalValue::List(_)] => Ok(MalValue::True),
+        [_] => Ok(MalValue::False),
+        _ => Err(anyhow!(
+            "Invalid number of args for list?, got {}",
+            args.len()
         )),
     }
 }
 
 pub fn is_empty(args: &Vec<MalValue>) -> Result<MalValue> {
     match &args[..] {
-        [MalValue::Number(l), MalValue::Number(r)] => Ok(MalValue::Number(l / r)),
+        [MalValue::List(l)] => match l.is_empty() {
+            true => Ok(MalValue::True),
+            false => Ok(MalValue::False),
+        },
         a => Err(anyhow!(
-            "Cannot div args: {:?}",
+            "Cannot check is empty, too many args or not a list: {:?}",
             a.iter().map(|v| pr_str(v)).collect::<Vec<_>>()
         )),
     }
@@ -84,9 +81,9 @@ pub fn is_empty(args: &Vec<MalValue>) -> Result<MalValue> {
 
 pub fn count(args: &Vec<MalValue>) -> Result<MalValue> {
     match &args[..] {
-        [MalValue::Number(l), MalValue::Number(r)] => Ok(MalValue::Number(l / r)),
+        [MalValue::List(l)] => Ok(MalValue::Number(l.len() as i64)),
         a => Err(anyhow!(
-            "Cannot div args: {:?}",
+            "Cannot get the count of  args: {:?}",
             a.iter().map(|v| pr_str(v)).collect::<Vec<_>>()
         )),
     }
@@ -104,10 +101,6 @@ pub fn eq(args: &Vec<MalValue>) -> Result<MalValue> {
 pub fn lt_eq(args: &Vec<MalValue>) -> Result<MalValue> {
     match &args[..] {
         [MalValue::Number(l), MalValue::Number(r)] => Ok(MalValue::Number(l / r)),
-        a => Err(anyhow!(
-            "Cannot div args: {:?}",
-            a.iter().map(|v| pr_str(v)).collect::<Vec<_>>()
-        )),
     }
 }
 pub fn lt(args: &Vec<MalValue>) -> Result<MalValue> {
